e5a6726f695de39b24fecf499c03d4d1234c9786
ChidoMunyanyi <119809704+ChidoMunyanyi@users.noreply.github.com>
2023-03-23 18:43:08 +0000

    movieApi function added

--- src/components/Movics/MovieAPI.js
+++ src/components/Movics/MovieAPI.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, {useEffect, useState} from "react"
+import {Card, CardHeader, CardBody} from "grommet"
+
+
+
 
 const MovieAPI = () => {
-    return (
-        <div>MovieAPI</div>
-    )
-}
+    const [topRated, setTopRated] = useState({
+        name: '',
+        plot: '',
+        poster: '',
+        releaseDate: '',
+    });
 
-export default MovieAPI
+        useEffect(() => {
+            fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=10754df5d1765ad1f1824e9434972268`)
+             .then((response) => response.json())
+             
+              .then(data => {
+                console.log(data)
+                
+                setTopRated({
+                    
+                    name: data.results[0].title,
+                    poster: data.results[0].poster_path,
+                    plot: data.results[0].overview,
+                    releaseDate: data.results[0].release_date,
+
+                });
+            });
+    }, [topRated]);
+
+   
+    return (
+        <div>
+            <Card height="medium" width="small" background="light-1">
+                <CardHeader pad="medium">{topRated.name}{topRated.releaseDate}</CardHeader>
+                <CardBody pad="medium">{topRated.poster}{topRated.plot}
+                    <img src="https://api.themoviedb.org/3/movie/{topRated.poster}" alt={topRated.title} /></CardBody>
+            </Card>
+        </div>
+    );
+    }
+
+    export default MovieAPI
