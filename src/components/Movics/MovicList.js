933eb7905973321ea0c96096e30e8140bae2d3a7
Yasha <yasha.zamanpour@gmail.com>
2023-03-24 18:30:44 +0000

    Separate music and movie list page

--- src/components/Movics/MovicList.js
+++ src/components/Movics/MovicList.js
@@ -1,12 +1,36 @@
 import React from 'react'
 import MovieAPI from './MovieAPI'
+import MusicAPI from './MusicAPI'
 
-const MovicList = () => {
-  return (
-    <div>Movie List
-      <div><MovieAPI/></div>
-    </div>
-  )
+const MovicList = (props) => {
+  if (props.type === "movie") {
+    return (
+      <div>
+        <h2>Movie List</h2>
+        <div><MovieAPI /></div>
+      </div>
+    )
+
+  } else if (props.type === "music") {
+    return (
+      <div>
+        <h2>Music List</h2>
+        <div><MusicAPI /></div>
+      </div>
+    )
+
+  } else {
+    return (
+      <div>
+        <h2>Movie List</h2>
+        <div><MovieAPI /></div>
+        <hr />
+        <h2>Music List</h2>
+        <div><MusicAPI /></div>
+      </div>
+    )
+
+  }
 }
 
 export default MovicList
