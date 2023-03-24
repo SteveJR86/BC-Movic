52bada1e38e5c07de902e7df220eae186de3a837
ChidoMunyanyi <119809704+ChidoMunyanyi@users.noreply.github.com>
2023-03-24 18:47:12 +0000

    starter add movie search API

--- src/components/Users/SuggestMovic.js
+++ src/components/Users/SuggestMovic.js
@@ -2,7 +2,21 @@ import React from 'react'
 
 const SuggestMovic = () => {
     return (
+
+        function searchMovie (movie) {
+        
+            fetch(`https://api.themoviedb.org/3/search/movie?=${movie}&api_key=10754df5d1765ad1f1824e9434972268&language=en-US&page=1&include_adult=false`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+            })
+    
+    
+        }
+    
         <div>SuggestMovic</div>
+
+
     )
 }
 
