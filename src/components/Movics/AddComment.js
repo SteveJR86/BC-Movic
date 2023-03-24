5bf139b9d9cb0a5aab0309fe30a84a9435fccd16
SteveJR86 <15844719+SteveJR86@users.noreply.github.com>
2023-03-24 19:02:36 +0000

    add comments component amended to only allow comments if logged in

--- src/components/Movics/AddComment.js
+++ src/components/Movics/AddComment.js
@@ -1,39 +1,53 @@
 import React, { useState } from 'react';
-import { Button, Form, FormField, TextArea } from 'grommet/components';
+import { Button, Card, Form, FormField, TextArea } from 'grommet/components';
 import { getAuth } from '@firebase/auth';
 import { getDatabase, ref, set } from "firebase/database";
 
-
-
-const AddComment = ({ app, movicid  }) => {
-    // Initialize Realtime Database and get a reference to the service
-    
+const AddCommentForm = ({ app, movicid }) => {
     const [value, setValue] = useState({
         "comment": ""
     })
     const auth = getAuth()
     return (
         <Form 
-            value={value}
-            onChange={nextValue => setValue(nextValue)}
-            onSubmit={({value}) => {
-                const database = getDatabase(app);
-                set(ref(database, "movic/" + movicid), {
-                    email: auth.currentUser.email,
-                    comment: value.comment
-                })
-                setValue({"comment": ""})
-            }}
-        >
-            <FormField name="comment" htmlFor="comment-area" label="Add new comment:">
-                <TextArea
-                    id="comment-area"
-                    name="comment"
-                    required
-                />
-            </FormField>
-            <Button type="submit" primary label="Add Comment" />
-        </Form>
+                value={value}
+                onChange={nextValue => setValue(nextValue)}
+                onSubmit={({value}) => {
+                    const database = getDatabase(app);
+                    set(ref(database, "movic/" + movicid), {
+                        email: auth.currentUser.email,
+                        comment: value.comment
+                    })
+                    setValue({"comment": ""})
+                }}
+            >
+                <FormField name="comment" htmlFor="comment-area" label="Add new comment:">
+                    <TextArea
+                        id="comment-area"
+                        name="comment"
+                        required
+                    />
+                </FormField>
+                <Button type="submit" primary label="Add Comment" />
+            </Form>
+    )
+}
+
+const PleaseLogIn = () => {
+    return (
+        <p>Please log in to add comments</p>
+    )
+}
+
+const AddComment = ({ app, movicid  }) => {
+    // Initialize Realtime Database and get a reference to the service
+    const auth = getAuth()
+    
+    return (
+        <Card style={{ padding:"10px", backgroundColor:"#DDDDDD"}}>
+            {(auth.currentUser ? <AddCommentForm app={app} movicid={movicid} /> : <PleaseLogIn />)}
+        </Card>
+        
             
     )
 }
