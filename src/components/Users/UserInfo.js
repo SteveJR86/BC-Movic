24078988b9987827711b6dcaed49b24b89bef766
ChidoMunyanyi <119809704+ChidoMunyanyi@users.noreply.github.com>
2023-03-26 20:34:13 +0100

    updated userprofile structure

--- src/components/Users/UserInfo.js
+++ src/components/Users/UserInfo.js
@@ -1,21 +1,30 @@
 import React from 'react';
-import { Avatar, Box, Heading} from 'grommet';
+import { Avatar, Box, Heading,Card} from 'grommet';
 import {  User } from "grommet-icons";
 
 
 const UserInfo = () => {
     return (
 
-    <><Box direction="row" gap="small">
+    <>
+       <Box direction="row" gap="large" align="end">
             <Avatar size="4xl" background="brand"><User color="text-strong" />
-            </Avatar><Heading >Username</Heading>
+            </Avatar><Heading >Chido</Heading>
         </Box>
-        <Box>
+
+        <Box pad="large" background="dark">
+         <Card >
         <h1>Favourite Movies</h1>
+        <p>No added favourites yet</p>
+        </Card>
         </Box>
-        <Box>
+        <Box pad="large">
+            <Card>
         <h1>Favourite Songs</h1>
+        <p>No added favourites yet</p>
+        </Card>
         </Box>
+
         </>
 
     )
