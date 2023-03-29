14802e6149cd80a62a9722b40ec8ed5f0a1aa3bd
Yasha <yasha.zamanpour@gmail.com>
2023-03-29 19:59:37 +0100

    change the navbar with using grommet

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,71 +1,41 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+
+import { Menu } from 'grommet';
+
 import './style.css';
-
-function IsLoggedIn({ loggedIn }) {
-  if (loggedIn) {
-    return (
-      <>
-        <li className="nav-item">
-          <NavLink
-            to="/profile"
-            end
-            className={({ isActive }) =>
-              isActive ? 'nav-link active' : 'nav-link'
-            }
-          >
-            Profile
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink
-            to="/logout"
-            className={({ isActive }) =>
-              isActive ? 'nav-link active' : 'nav-link'
-            }
-          >
-            Logout
-          </NavLink>
-        </li>
-      </>
-    )
-  } else {
-    return (
-      <>
-        <li className="nav-item">
-          <NavLink
-            to="/login"
-            className={({ isActive }) =>
-              isActive ? 'nav-link active' : 'nav-link'
-            }
-          >
-            Login
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink
-            to="/register"
-            className={({ isActive }) =>
-              isActive ? 'nav-link active' : 'nav-link'
-            }
-          >
-            Register
-          </NavLink>
-        </li>
-      </>
-    )
-  }
-}
-
 
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function Navbar({ loggedIn }) {
+  const navigate = useNavigate();
+  let menuItems = [
+    { label: 'Home', onClick: () => { navigate("/") } },
+    { label: 'Movie list', onClick: () => { navigate("/movies") } },
+    { label: 'Music List', onClick: () => { navigate("/musics") } },
+    { label: 'About Movic', onClick: () => { navigate("/about") } },
+  ];
+  if (loggedIn) {
+    menuItems.push(
+      { label: 'Profile', onClick: () => { navigate("/profile") } },
+      { label: 'Logout', onClick: () => { navigate("/logout") } }
+    );
+  } else {
+    menuItems.push(
+      { label: 'Login', onClick: () => { navigate("/login") } },
+      { label: 'Register', onClick: () => { navigate("/register") } }
+    )
+  }
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <div className="navbar navbar-expand-lg navbar-light bg-light">
       <Link className="navbar-brand" to="/">
         Movic
       </Link>
-      <div>
+      <Menu
+        label="Menu"
+        items={menuItems}
+      />
+      {/* <div>
         <ul className="navbar-nav">
           <li className="nav-item">
             <NavLink
@@ -110,8 +80,8 @@ function Navbar({ loggedIn }) {
           </li>
           <IsLoggedIn loggedIn={loggedIn} />
         </ul>
-      </div>
-    </nav>
+      </div> */}
+    </div>
   );
 }
 
