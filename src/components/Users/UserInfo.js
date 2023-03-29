a190d1762a00331578843e434af29e275d8475b4
SteveJR86 <15844719+SteveJR86@users.noreply.github.com>
2023-03-29 19:30:34 +0100

    updated userinfo to use first initial of displayname if profile photo not set

--- src/components/Users/UserInfo.js
+++ src/components/Users/UserInfo.js
@@ -4,13 +4,14 @@ import { getAuth } from 'firebase/auth';
 
 
 const UserInfo = () => {
-    const user = getAuth().currentUser;
+    const user = getAuth();
+    
     return (
 
     <>
        <Box direction="row" gap="large" align="end">
-            <Avatar size="4xl" background="brand" src={user.photoURL}>
-            </Avatar><Heading >{user.displayName}</Heading>
+       {user.currentUser.photoURL ? <Avatar size='4xl' src={user.currentUser.photoURL} /> : <Avatar size='4xl' background='dark-2'>{user.currentUser.displayName[0]}</Avatar>}
+            <Heading >{user.currentUser.displayName}</Heading>
         </Box>
 
         <Box pad="large" background="dark">
